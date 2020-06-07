7c2fa45d9f50b3042a10b8a1c760dd4a358b0e6c
Gustavo Maciel <gustamaciel2012@hotmail.com>
2020-06-06 23:15:43 -0300

    Starting the app

--- todo.js
+++ todo.js
@@ -1,1 +1,5 @@
-alert("working!");
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var todos = ["Wake up", "Coffe", "Study"];
